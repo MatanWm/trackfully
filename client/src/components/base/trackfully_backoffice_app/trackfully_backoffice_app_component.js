10871e8363fbd1b20f3acc1e72874220171bdd2b
Matan Cohen <matan.c@walkme.com>
2018-08-05 16:07:56 +0300

    Adding image logo

--- client/src/components/base/trackfully_backoffice_app/trackfully_backoffice_app_component.js
+++ client/src/components/base/trackfully_backoffice_app/trackfully_backoffice_app_component.js
@@ -1,16 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
+
 import Main from '../main/main_component';
 
-class TrackfullyBackofficeApp extends Component {
-  constructor() {
-    super();
-
-    this.state = {};
-  }
-
-  render() {
-    return <Main />;
-  }
-}
+const TrackfullyBackofficeApp = () => <Main />;
 
 export default TrackfullyBackofficeApp;
--- client/src/pages/dashboard_page/dashboard_page_component.js
+++ client/src/pages/dashboard_page/dashboard_page_component.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import DashboardHighlights from '../../components/dashboard_highlights/dashboard_highlights_component';
 import DolevsList from '../../components/dolevs_list/dolevs_list_component';
+import DolevsStatus from '../../components/dolevs_status/dolev_status_component';
+
 import './dashboard_page.css';
 
-const CLASS_NAME = 'dashboard-page';
+const Header = () => (
+  <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <ul className="navbar-nav">
+        <li className="navbar-brand">
+          <Link className="nav-link " to="/backoffice/admin/new">
+            <img src={'./assets/img/leket_logo.png'} />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    <hr />
+  </div>
+);
 
 class DashboardPage extends React.Component {
   constructor() {
@@ -22,12 +38,12 @@ class DashboardPage extends React.Component {
   }
 
   render() {
-    console.log(this.state.dolves);
     return (
-      <div className={CLASS_NAME}>
-        <div>Hello Trackfully</div>
+      <div className="container-fluid">
+        <Header />
         <DashboardHighlights />
         <DolevsList />
+        <DolevsStatus dolevs={this.state.dolevs} />
       </div>
     );
   }
