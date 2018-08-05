2b2a00161c455bec98e9a14185c29447561a4ad5
Gadi Raymond <gadi.r@walkme.com>
2018-08-05 15:03:57 +0300

    update dolev component

--- client/src/components/base/main/main_component.js
+++ client/src/components/base/main/main_component.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import DashboardPage from '../../../pages/dashboard_page/dashboard_page_component';
+import UpdateDolev from '../../../pages/dolev/update_dolev_component';
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={DashboardPage}/>
       <Route exact path='/sample' component={DashboardPage}/>
+      <Route exact path='/dolev/:id' component={UpdateDolev}/>
     </Switch>
   </main>
 );
