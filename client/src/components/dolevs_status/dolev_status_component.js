4cef20299873d09a77f2c5b9000b826afad1f11e
Matan Cohen <matan.c@walkme.com>
2018-08-05 16:08:22 +0300

    more changes

---
+++ client/src/components/dolevs_status/dolev_status_component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CLASS_NAME = 'dolevs-list';
+
+const DolevsList = () => (
+  <div className="col-lg-6">
+    <div>Hello DolevsList</div>
+  </div>
+);
+
+export default DolevsList;
