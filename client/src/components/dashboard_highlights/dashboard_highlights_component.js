500146efc2760681d5631544bec54510f1362677
WALKME\maxim.p <maxim.p@walkme.com>
2018-08-05 18:34:25 +0300

    fix stuff

--- client/src/components/dashboard_highlights/dashboard_highlights_component.js
+++ client/src/components/dashboard_highlights/dashboard_highlights_component.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import Highligh from './highlight'
-
-const CLASS_NAME = 'Dashboard-highlights';
+import Highlight from './highlight'
 
 const IDLE_STATUS = 0;
 const IN_FIELD_STATUS = 1;
@@ -9,29 +7,28 @@ const FRIDGE_STATUS = 6;
 const ASSOCIATION_STATUS = 8;
 
 const DashboardHighlights = (props) => {
-  const dolevsByStatusObject = getNumOfDolevsByStatusObject(props.dolevs);
   return (
     <div className='hilights-block'>
-      <Highligh name='דולבים פנויים'
-                numOfDolevs={allDolevs.filter(dolev => dolev.status === IDLE_STATUS).length}
-                iconName = 'dolev'
-                buttonClass='badge badge-success'
-                handleHighlightClick={() => props.onHighlightClick(IDLE_STATUS)}/>
-      <Highligh name='דולבים בשטח'
-                iconName = 'wheat'
-                buttonClass='badge badge-warning'
-                numOfDolevs={allDolevs.filter(dolev => dolev.status === IN_FIELD_STATUS).length}
-                handleHighlightClick={() => props.onHighlightClick(IDLE_STATUS)}/>
-      <Highligh name='בקירור'
-                iconName='fridge'
-                buttonClass='badge badge-info'
-                numOfDolevs={allDolevs.filter(dolev => dolev.status === FRIDGE_STATUS).length}
-                handleHighlightClick={() => props.onHighlightClick(FRIDGE_STATUS)}/>
-      <Highligh name='בעמותה'
-                iconName = 'amuta'
-                buttonClass='badge badge-primary'
-                numOfDolevs={allDolevs.filter(dolev => dolev.status === ASSOCIATION_STATUS).length}
-                handleHighlightClick={() => props.onHighlightClick('ASSOCIATION_STATUS')}/>
+      <Highlight name='דולבים פנויים'
+                 numOfDolevs={props.dolevs.filter(dolev => dolev.status === IDLE_STATUS).length}
+                 iconName = 'dolev'
+                 buttonClass='badge badge-success'
+                 handleHighlightClick={() => props.onHighlightClick(IDLE_STATUS)}/>
+      <Highlight name='דולבים בשטח'
+                 iconName = 'wheat'
+                 buttonClass='badge badge-warning'
+                 numOfDolevs={props.dolevs.filter(dolev => dolev.status === IN_FIELD_STATUS).length}
+                 handleHighlightClick={() => props.onHighlightClick(IDLE_STATUS)}/>
+      <Highlight name='בקירור'
+                 iconName='fridge'
+                 buttonClass='badge badge-info'
+                 numOfDolevs={props.dolevs.filter(dolev => dolev.status === FRIDGE_STATUS).length}
+                 handleHighlightClick={() => props.onHighlightClick(FRIDGE_STATUS)}/>
+      <Highlight name='בעמותה'
+                 iconName = 'amuta'
+                 buttonClass='badge badge-primary'
+                 numOfDolevs={props.dolevs.filter(dolev => dolev.status === ASSOCIATION_STATUS).length}
+                 handleHighlightClick={() => props.onHighlightClick('ASSOCIATION_STATUS')}/>
     </div>
   );
 };
