caac89521cda5307477ef840ddc7f6285c8c06d3
Matan Cohen <matan.c@walkme.com>
2018-08-05 15:21:18 +0300

    Adding get dolev in client side

--- client/src/pages/dashboard_page/dashboard_page_component.js
+++ client/src/pages/dashboard_page/dashboard_page_component.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import axios from 'axios';
+
 import DashboardHighlights from '../../components/dashboard_highlights/dashboard_highlights_component';
 import DolevsList from '../../components/dolevs_list/dolevs_list_component';
 import './dashboard_page.css';
 
 const CLASS_NAME = 'dashboard-page';
 
-const DashboardPage = () => (
-  <div className={CLASS_NAME}>
-    <div>Hello Trackfully</div>
-    <DashboardHighlights />
-    <DolevsList />
-  </div>
-);
+class DashboardPage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8080/api/dolev').then((res) => {
+      const dolves = res.data;
+      this.setState({ dolves });
+    });
+  }
+
+  render() {
+    console.log(this.state.dolves);
+    return (
+      <div className={CLASS_NAME}>
+        <div>Hello Trackfully</div>
+        <DashboardHighlights />
+        <DolevsList />
+      </div>
+    );
+  }
+}
 
 export default DashboardPage;
