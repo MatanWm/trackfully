eff05f0a7020782ea6ae8b43b4d8b22bc9aa605d
Matan Cohen <matan.c@walkme.com>
2018-08-06 12:32:59 +0300

    Fixing docker prod

--- client/src/pages/update_dolev/update_dolev_page.js
+++ client/src/pages/update_dolev/update_dolev_page.js
@@ -11,7 +11,7 @@ class DashboardPage extends React.Component {
   }
 
   onSubmit() {
-    axios.get('http://localhost:8080/api/dolev').then((res) => {
+    axios.get('http://trackfully.westeurope.cloudapp.azure.com:8080/api/dolev').then((res) => {
       const dolevs = res.data;
       this.setState({ dolevs });
     });
